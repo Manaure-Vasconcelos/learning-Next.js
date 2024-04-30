b9d4fb842dff2f8ca895a436f54cd1f8bac49a3d
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-04-30 17:49:56 -0300

    style: styled home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,17 @@ async function HomePage() {
   const notes = await loadNotes();
 
   return (
-    <div>
-      <NoteForm/>
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <NoteForm />
 
-      {notes.map((note) => (
-        <div key={note.id}>
-          <h1>{note.title}</h1>
-          <p>{note.content}</p>
-        </div>
-      ))}
+        {notes.map((note) => (
+          <div key={note.id} className="bg-slate-400 p-4 my-2">
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
